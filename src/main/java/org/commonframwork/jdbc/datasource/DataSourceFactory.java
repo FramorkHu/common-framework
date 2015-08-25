6041a3de996057f5546724a4f41abfffb12cc989
huyan <huyan@cd.tuan800.com>
2015-08-25 18:43:07 +0800

    up

---
+++ src/main/java/org/commonframwork/jdbc/datasource/DataSourceFactory.java
@@ -0,0 +1,12 @@
+package org.commonframwork.jdbc.datasource;
+
+import javax.sql.DataSource;
+
+/**
+ * Created by huyan on 2015/8/25.
+ */
+public interface DataSourceFactory {
+
+    public DataSource getDataSource();
+
+}
